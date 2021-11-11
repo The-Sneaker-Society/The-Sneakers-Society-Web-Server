9bde73fe92e7f9b8ed14b75798203bc8f85db10c
Rabin Adhikari <48793377+rabin1995@users.noreply.github.com>
2021-11-11 17:51:54 -0500

    Create index.js

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app=express();
+const mysql = require("mysql");
+
+const db = mysql.createConnection ({
+    host: "localhost",
+    user: "root",
+    password: "password",
+    database: "the_sneakers_society_mysql_database",
+});
+
+app.get("/", (req, res) => {
+    const sqlInsert = "INSERT INTO the_sneakers_society (ColumnName, Comments) VALUES ('columnnam', 'comments');";
+    db.query(sqlInsert, (err, result)=> {
+    res.send("The-Sneakers-Society-Web-Application");
+});
+});
+
+app.listen(3001, ()=> {
+    console.log("The application is running.");
+});
